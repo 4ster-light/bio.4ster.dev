2bf7848ca5f213d586bc8d86fc7e6746ff2bef60
4ster-light <4ster.dev@proton.me>
2026-02-27 18:56:42 +0100

    fix: switched to deno for project management

--- src/main.tsx
+++ src/main.tsx
@@ -2,5 +2,5 @@ import { render } from "preact"
 import App from "@/App.tsx"
 import "@/assets/app.css"
 
-const container = document.getElementById("app")!
+const container = document.getElementById("app") as HTMLElement
 render(<App />, container)
